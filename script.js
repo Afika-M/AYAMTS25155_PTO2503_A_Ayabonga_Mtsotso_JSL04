4316c71ae3c87059f49c4fe348312d334fad508e
Ayabonga Mtsotso <ayabonga.afika.mtsotso@gmail.com>
2025-08-31 10:04:03 +0200

    Function to add tasks added

---
+++ script.js
@@ -0,0 +1,60 @@
+const initialTasks = [
+  {
+    id: 1,
+    title: "Launch Epic Career 🚀",
+    description: "Create a killer Resume",
+    status: "todo",
+  },
+  {
+    id: 2,
+    title: "Master JavaScript 💛",
+    description: "Get comfortable with the fundamentals",
+    status: "doing",
+  },
+  {
+    id: 3,
+    title: "Keep on Going 🏆",
+    description: "You're almost there",
+    status: "doing",
+  },
+  {
+    id: 11,
+    title: "Learn Data Structures and Algorithms 📚",
+    description:
+      "Study fundamental data structures and algorithms to solve coding problems efficiently",
+    status: "todo",
+  },
+  {
+    id: 12,
+    title: "Contribute to Open Source Projects 🌐",
+    description:
+      "Gain practical experience and collaborate with others in the software development community",
+    status: "done",
+  },
+  {
+    id: 13,
+    title: "Build Portfolio Projects 🛠️",
+    description:
+      "Create a portfolio showcasing your skills and projects to potential employers",
+    status: "done",
+  },
+];
+
+/* Adding tasks */
+
+function addTask(tasks) {
+  tasks.forEach((task) => {
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task-div");
+
+    const titleEl = document.createElement("h4");
+    titleEl.textContent = task.title;
+
+    const descEl = document.createElement("p");
+    descEl.textContent = task.description;
+
+    taskDiv.append(titleEl);
+    taskDiv.append(descEl);
+  });
+}
+addTask(initialTasks);
